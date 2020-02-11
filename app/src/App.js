c6b4f031653a1d2c9cf652997762c7295fd4bd24
mohigh999 <pierre.monier99@gmail.com>
2020-02-11 09:59:21 +0100

    FInish the navigation function

--- app/src/App.js
+++ app/src/App.js
@@ -16,76 +16,102 @@ export default class App extends Component {
     this.addHammer = this.addHammer.bind(this)
     this.state = {
       current_view: 0,
-      tab_view: ["#card-list", "#accordeur", "#metronome"]
+      tab_view: ["#card-list", "#accordeur", "#metronome"],
     }
-  }
 
+  }
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// ComponentDidMount
   componentDidMount() {
+
     const app = document.querySelector(".App")
     this.addHammer(app)
     this.setState({ 
       view: document.querySelectorAll(".view"),
       nbr_view: app.childNodes.length - 2
     });
+    this.viewOutDisplayer(this.state.current)
 
   }
-
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// AddHamer
   addHammer(app) {
     
     this.hammer = new Hammer(app) 
     this.hammer.on("swiperight", () => {
       if(this.state.current_view > 0){
-        let old = "#"+this.state.view[this.state.current_view].getAttribute("id")
         this.setState( state => {
           // eslint-disable-next-line
           current_view: state.current_view--
         })
         let target = "#"+this.state.view[this.state.current_view].getAttribute("id")
-        this.nav(target, old)
-        document.querySelector(old).style.transform = "translateX("+window.innerWidth+"px)";
+        this.nav(target)
       }
-
     })
     this.hammer.on("swipeleft", () => {
       if(this.state.current_view < this.state.nbr_view){
-        let old = "#"+this.state.view[this.state.current_view].getAttribute("id")
         this.setState( state => {
           // eslint-disable-next-line
           current_view: state.current_view++
         })
         let target = "#"+this.state.view[this.state.current_view].getAttribute("id")
-        this.nav(target, old)
-        document.querySelector(old).style.transform = "translateX("+(-window.innerWidth)+"px)";
+        this.nav(target)
       }
     })
-  }
 
-  nav(target, old) {
-    if(!old){
-      
-      let old = "#"+this.state.view[this.state.current_view].getAttribute("id")
-      
-      this.setState({
-        current_view: this.state.tab_view.indexOf(target),
-      })
-      if(this.state.tab_view.indexOf(target) - this.state.tab_view.indexOf(old) < 0){      
-        document.querySelector(old).style.transform = "translateX("+(window.innerWidth)+"px)";
-      }else{
-        document.querySelector(old).style.transform = "translateX("+(-window.innerWidth)+"px)";
-      }
-    }
-   
+  }
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// Nav
+  nav(target) {
+
     this.state.view.forEach(el => {
       el.classList.remove("view-in")
       el.classList.add("view-out")
     })
+    
     document.querySelector(target).classList.remove("view-out")
     document.querySelector(target).classList.add("view-in")
     document.querySelector(target).style.transform = "translateX(0px)";
-    
-  }
+    this.viewOutDisplayer(this.state.tab_view.indexOf(target))
 
+  }
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// VIewOutDisplayer
+  viewOutDisplayer(target) {
+
+    let viewout = document.querySelectorAll(".view-out")
+    switch(target){
+      case 0:
+        viewout.forEach(el => {
+          el.style.transform = "translateX("+(window.innerWidth)+"px)";
+        })
+        break;
+      case this.state.tab_view.length - 1:
+        viewout.forEach(el => {
+          el.style.transform = "translateX("+(-window.innerWidth)+"px)";
+        })
+        break;
+      default:
+        viewout.forEach((el, i) => {
+          if(i < target){
+            el.style.transform = "translateX("+(-window.innerWidth)+"px)";
+          }else{
+            el.style.transform = "translateX("+(window.innerWidth)+"px)";
+          }
+        })
+        break;
+    }
+
+  }
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+// Render, old, false
   render() {
+
     return (
       <div className="App" data-testid="app">
         <CardList data-nbr="0">></CardList>
@@ -94,5 +120,6 @@ export default class App extends Component {
         <AppNavigation func={this.nav}></AppNavigation>
       </div>
     );
+
   }
 }
