b663461007a8ce2060519218efca30cce3be9d36
mohigh999 <pierre.monier99@gmail.com>
2020-02-12 16:24:21 +0100

    Remove useless swipe navigation, pan sys work fine

--- app/src/App.js
+++ app/src/App.js
@@ -27,12 +27,13 @@ export default class App extends Component {
   componentDidMount() {
 
     const app = document.querySelector(".App")
+    const appview = document.querySelector("#allview")
     this.addHammer(app)
     this.setState({ 
       view: document.querySelectorAll(".view"),
-      nbr_view: app.childNodes.length - 2
+      nbr_view: appview.childNodes.length
     });
-    this.viewOutDisplayer(this.state.current)
+    this.viewOutDisplayer(this.state.current_view)
 
   }
 // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
@@ -41,59 +42,61 @@ export default class App extends Component {
   addHammer(app) {
     
     this.hammer = new Hammer(app) 
-
-
     this.hammer.on("panleft", (e) => {
-      console.log(this.state.current_view, (this.state.tab_view.length -1))
+      let tsl_current = (-this.state.current_view * window.innerWidth) + e.deltaX
       if(this.state.current_view === (this.state.tab_view.length -1)){
-        console.log(e.deltaX, (window.innerWidth/2))
         if(Math.abs(e.deltaX) < (window.innerWidth/2)){
-          document.querySelector("#allview").style.transform = "translateX("+e.deltaX+"px)";
+          document.querySelector("#allview").classList.add("no-transi")
+          document.querySelector("#allview").style.transform = "translateX("+tsl_current+"px)";
         }    
       }else{
-        document.querySelector("#allview").style.transform = "translateX("+e.deltaX+"px)";
+        document.querySelector("#allview").classList.add("no-transi") 
+        document.querySelector("#allview").style.transform = "translateX("+tsl_current+"px)";
       }
     })
-
-
+    this.hammer.on("panright", (e) => {
+      let tsl_current = (-this.state.current_view * window.innerWidth) + e.deltaX
+      if(this.state.current_view === 0){
+        if(Math.abs(e.deltaX) < (window.innerWidth/2)){
+          document.querySelector("#allview").classList.add("no-transi")
+          document.querySelector("#allview").style.transform = "translateX("+tsl_current+"px)";
+        }    
+      }else{
+        document.querySelector("#allview").classList.add("no-transi")
+        document.querySelector("#allview").style.transform = "translateX("+tsl_current+"px)";
+      }
+    })
     this.hammer.on("panend", (e) => {
-      console.log(e)
-      let current = "#"+this.state.view[this.state.current_view].getAttribute("id")
+      document.querySelector("#allview").classList.remove("no-transi")
       // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
       // Droite
-      console.log(Math.abs(e.deltaX), )
       if(Math.abs(e.deltaX) > (window.innerWidth/2) && this.state.current_view < (this.state.tab_view.length -1) && e.deltaX < 0){
-        
+        console.log("DROITE IF")
         let target = "#"+this.state.view[this.state.current_view+1].getAttribute("id")
         this.nav(target)
 
       }
-      document.querySelector("#allview").style.transform = "translateX(0px)";
-    })
-
-
-    
-    this.hammer.on("swiperight", () => {
-      if(this.state.current_view > 0){
-        this.setState( state => {
-          // eslint-disable-next-line
-          current_view: state.current_view--
-        })
-        let target = "#"+this.state.view[this.state.current_view].getAttribute("id")
+      else if(this.state.current_view === (this.state.tab_view.length -1)){
+        console.log("DROITE ELSE")
+        document.querySelector("#allview").style.transform = "translateX("+(-this.state.current_view) * window.innerWidth+"px)";
+      }else{
+        // Milieu dir = droite
+        document.querySelector("#allview").style.transform = "translateX("+(-this.state.current_view) * window.innerWidth+"px)";
+      }
+      // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+      // Gauche
+      if(Math.abs(e.deltaX) > (window.innerWidth/2) && this.state.current_view !== 0 && e.deltaX > 0){
+        console.log("GAUCHE IF")
+        let target = "#"+this.state.view[this.state.current_view-1].getAttribute("id")
         this.nav(target)
+      } else if(this.state.current_view === 0){
+        console.log("GAUCHE ELSE")
+        document.querySelector("#allview").style.transform = "translateX(0px)";
+      }else{
+        // Milieu dir = gauche
+        document.querySelector("#allview").style.transform = "translateX("+(-this.state.current_view) * window.innerWidth+"px)";
       }
     })
-    this.hammer.on("swipeleft", () => {
-      if(this.state.current_view < this.state.nbr_view){
-        this.setState( state => {
-          // eslint-disable-next-line
-          current_view: state.current_view++
-        })
-        let target = "#"+this.state.view[this.state.current_view].getAttribute("id")
-        this.nav(target)
-      }
-    })
-
   }
 // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
 // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
@@ -101,7 +104,6 @@ export default class App extends Component {
   nav(target) {
 
     let active_nav_item = this.state.tab_view.indexOf(target)
-
     this.state.view.forEach(el => {
       el.classList.remove("view-in")
       el.classList.add("view-out")
@@ -109,28 +111,30 @@ export default class App extends Component {
     this.setState({
       current_view: this.state.tab_view.indexOf(target)
     })
+
     document.querySelector(target).classList.remove("view-out")
     document.querySelector(target).classList.add("view-in")
-    document.querySelector(target).style.transform = "translateX(0px)";
-    this.viewOutDisplayer(this.state.tab_view.indexOf(target))
+    document.querySelector("#allview").style.transform = "translateX("+ - (active_nav_item * window.innerWidth)+"px)";
     this.changeBottomNaviationIndice(active_nav_item)
+    // this.viewOutDisplayer(this.state.tab_view.indexOf(target))
 
   } 
 // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
 // °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
 // viewOutDisplayer
   viewOutDisplayer(target) {
-
     let viewout = document.querySelectorAll(".view-out")
     switch(target){
       case 0:
-        viewout.forEach(el => {
-          el.style.transform = "translateX("+(window.innerWidth)+"px)";
+        viewout.forEach((el,i) => {
+          el.style.transform = "translateX("+(window.innerWidth * (i+1))+"px)";
         })
         break;
       case this.state.tab_view.length - 1:
+        let inver_indice = viewout.length
         viewout.forEach(el => {
-          el.style.transform = "translateX("+(-window.innerWidth)+"px)";
+          el.style.transform = "translateX("+(-window.innerWidth * inver_indice)+"px)";
+          inver_indice--
         })
         break;
       default:
